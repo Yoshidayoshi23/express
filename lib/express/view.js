Yoshidayoshi23/express

commit 1c2e9a476c50640ea4a15700727156b8da9008dd

Fixed _ prefix object resolution on partials (resolveObjectName), added _ prefix fallback to view file resolution (prefixPath)

lib/express/view.js
@@ -198,13 +198,20 @@ http.ServerResponse.prototype.render = function(view, options, fn, parent){
     ? new Partial(view, options)
     : new View(view, options);
 
-  // Ensure view exists, otherwise try index
+  // Ensure view exists, otherwise try _ prefix
+  if (!view.exists) {
+    view = partial
+      ? new Partial(view.prefixPath, options)
+      : new View(view.prefixPath, options);
+  }
+  
+  // Ensure view _ prefix exists, otherwise try index
   if (!view.exists) {
     view = partial
       ? new Partial(view.indexPath, options)
       : new View(view.indexPath, options);
   }
-
+  
   // Dynamic helper support
   if (false !== options.dynamicHelpers) {
     // cache

lib/express/view/view.js
@@ -148,6 +148,17 @@ View.prototype.__defineGetter__('indexPath', function(){
 });
 
 /**
+ * Return _ prefix path alternative
+ *
+ * @return {String}
+ * @api public
+ */
+
+View.prototype.__defineGetter__('prefixPath', function(){
+  return this.dirname + '/_' + this.basename;
+});
+
+/**
  * Register the given template engine `exports`
  * as `ext`. For example we may wish to map ".html"
  * files to jade:

lib/express/view/partial.js
@@ -56,6 +56,7 @@ exports.resolveObjectName = function(view){
     .split('/')
     .slice(-1)[0]
     .split('.')[0]
+    .replace(/^_/, '')
     .replace(/[^a-zA-Z0-9 ]+/g, ' ')
     .split(/ +/).map(function(word, i){
       return i

commit 8e6229a870dda3167a171ed0260bc5b3148480cb

Fixed index view resolution bug introduced by 1c2e9a476c50

lib/express/view.js
@@ -194,7 +194,7 @@ http.ServerResponse.prototype.render = function(view, options, fn, parent){
 
   // Populate view
   // TODO: cache
-  view = partial
+  var orig = view = partial
     ? new Partial(view, options)
     : new View(view, options);
 
@@ -208,8 +208,8 @@ http.ServerResponse.prototype.render = function(view, options, fn, parent){
   // Ensure view _ prefix exists, otherwise try index
   if (!view.exists) {
     view = partial
-      ? new Partial(view.indexPath, options)
-      : new View(view.indexPath, options);
+      ? new Partial(orig.indexPath, options)
+      : new View(orig.indexPath, options);
   }
   
   // Dynamic helper support